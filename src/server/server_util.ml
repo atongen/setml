d711cbd9fa0f5ce238c5401f540b1124deddb600
Andrew Tongen <atongen@gmail.com>
2018-04-28 08:28:31 -0500

    Add generic base-number conversions

--- src/server/server_util.ml
+++ src/server/server_util.ml
@@ -1,6 +1,6 @@
 open CCList.Infix
 
-let chars = (48 -- 57) @ (97 -- 122) >|= Char.chr |> CCArray.of_list
+let chars = (48 -- 57) @ (97 -- 122) @ (65 -- 90) >|= Char.chr |> Array.of_list
 
 let form_decode body = Uri.query_of_encoded body
 
@@ -17,29 +17,41 @@ let string_to_char_list s =
       aux (s.[i] :: ac) (pred i)
   in aux [] (String.length s - 1)
 
-(* decode from base-36 positive string to base-10 integer *)
-let int_of_base36 b =
-  let rec aux b pos ac =
-    match b with
+let chars_of_base b =
+    if 0 <= b && b < Array.length chars then
+        Array.sub chars 0 b
+    else
+        raise (Invalid_argument ("Invalid base value: " ^ string_of_int b))
+
+let int_of_base s b =
+  let my_chars = chars_of_base b in
+  let rec aux l pos ac =
+    match l with
     | [] -> ac
     | hd :: tl ->
-      match CCArray.find_idx (fun x -> x = hd) chars with
-      | Some ((idx, x)) -> aux tl (pos+1) (ac + (idx * (CCInt.pow 36 pos)))
-      | None -> raise (Invalid_argument ("Invalid base36 char: " ^ String.make 1 hd))
+      match CCArray.find_idx (fun x -> x = hd) my_chars with
+      | Some ((idx, x)) -> aux tl (pos+1) (ac + (idx * (CCInt.pow b pos)))
+      | None -> raise (Invalid_argument ("Invalid base " ^ string_of_int b ^ " char: " ^ String.make 1 hd))
   in
-  aux (CCList.rev (string_to_char_list b)) 0 0
+  aux (CCList.rev (string_to_char_list s)) 0 0
 
-(* encode from base-10 positive integer to base-36 string *)
-let base36_of_int n =
+(* decode from base-36 positive string to base-10 integer *)
+let int_of_base36 s = int_of_base s 36
+
+let base_of_int n b =
+  let my_chars = chars_of_base b in
   let rec aux n ac =
     if n < 0 then
       raise (Invalid_argument ("Negative values unsupported"))
     else if n = 0 then
-      if ac = "" then "0" else ac
+      if ac = "" then (String.make 1 (my_chars.(0))) else ac
     else
-      let next = n / 36 in
-      let remainder = n mod 36 in
-      let chr = chars.(remainder) in
+      let next = n / b in
+      let remainder = n mod b in
+      let chr = my_chars.(remainder) in
       aux next (String.make 1 chr) ^ ac
   in
   aux n ""
+
+(* encode from base-10 positive integer to base-36 string *)
+let base36_of_int n = base_of_int n 36
