fe343b13f95e83e5aec7930e5ad0edb031cf596a
Andrew Tongen <atongen@gmail.com>
2018-10-14 14:42:01 -0500

    Update code launch script

--- script/code.rb
+++ script/code.rb
@@ -1,23 +1,37 @@
 #!/usr/bin/env ruby
 
+dir = File.expand_path("../..", __FILE__)
+Dir.chdir(dir)
+
+def select_parts(parts, strs)
+  regexs = strs.map { |s| %r(#{s}) }
+  parts.select do |p|
+    regexs.any? { |r| r.match(p) } &&
+      File.exists?(p)
+  end
+end
+
 parts = ENV["PATH"].split(":")
-opam = parts.detect { |p| p =~ /opam/ }
-nvm = parts.detect { |p| p =~ /nvm/ }
-parts.delete(opam)
-parts.delete(nvm)
-parts.uniq!
+
+ocaml = select_parts(parts, %w( opam ocaml ))
+node_bin = File.expand_path("node_modules/.bin")
+js = select_parts((parts + [node_bin]), %w( nvm node ))
+rest = parts - (ocaml + js)
 
 env = case ARGV[0]
-when "opam"
-  ([opam, nvm] + parts)
-when "nvm"
-  ([nvm, opam] + parts)
+when "ocaml"
+  (ocaml + js + rest).uniq
+when "js"
+  (js + ocaml + rest).uniq
 else
-  STDERR.puts "arg must be 'opam' or 'nvm'"
+  []
+end
+
+if env.empty?
+  STDERR.puts "arg must be 'ocaml' or 'js'"
   exit 1
 end
 
 code = `which code`.strip
-dir = File.expand_path("../..", __FILE__)
-Dir.chdir(dir)
-system({ "PATH" => env.join(":") }, code)
+path = env.join(":")
+system({ "PATH" => path }, code)
