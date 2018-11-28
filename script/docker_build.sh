464fdc456cf2a374694d76a42c79ea41bf698e78
Andrew Tongen <atongen@gmail.com>
2018-11-28 10:09:39 -0600

    Move styles to assets folder

--- script/docker_build.sh
+++ script/docker_build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+repo=atongen
 name=setml
+
 version=`cat ${name}.opam | egrep '^version: ' | cut -d '"' -f2`
-image="`whoami`/${name}:${version}"
+image="${repo}/${name}:${version}"
 
 docker build -t "$image" .
