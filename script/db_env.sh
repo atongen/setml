f350533629b30e15b8410cd2a686a2618432a9b3
Andrew Tongen <atongen@gmail.com>
2018-12-18 14:06:00 -0600

    Update env parsing

--- script/db_env.sh
+++ script/db_env.sh
@@ -2,16 +2,26 @@
 
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-SETML_ENV=${SETML_ENV:-development}
-DB_NAME=${DB_NAME:-setml_${SETML_ENV}}
-DB_HOST=${DB_HOST:-localhost}
-DB_PORT=${DB_PORT:-5432}
-DB_USER=${DB_USER:-`whoami`}
-DB_PASS=${DB_PASS:-abcd1234}
-DB_POOL=${DB_POOL:-`cat /proc/cpuinfo | grep processor | wc -l`}
+# capture current env
+tmpenv=$(mktemp /tmp/`basename $0`.XXXXXXXXXX)
+trap "rm -f $tmpenv" EXIT
+env | sed 's/=/="/' | sed 's/$/"/' > "$tmpenv"
 
+# set default values - lowest priority
+SETML_ENV="development"
+DB_NAME="setml_${SETML_ENV}"
+DB_HOST="localhost"
+DB_PORT="5432"
+DB_USER="`whoami`"
+DB_PASS="abcd1234"
+DB_POOL="`cat /proc/cpuinfo | grep processor | wc -l`"
+
+# source envfile - medium priority
 ENVFILE=${ENVFILE:-envfile}
 [[ -f "$ENVFILE" ]] && source "${ENVFILE}"
+
+# source current env - highest priority
+source "$tmpenv"
 
 echo "SETML_ENV=$SETML_ENV \
 DB_NAME=$DB_NAME \
